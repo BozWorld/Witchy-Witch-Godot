f107449cf6a2146ac72775f627c5de82593bbf46
Drasibaoh <boisardnoah@gmail.com>
2024-07-14 11:39:32 +0200

    Update enemy_spawning.gd

--- Scene/Enemy/enemy_spawning.gd
+++ Scene/Enemy/enemy_spawning.gd
@@ -2,17 +2,21 @@ extends Node2D
 
 @export var mob_scene: PackedScene
 var spawnPoints : Array = []
+var rand = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass # Replace with function body.
+func _spawn():
 	var screenSize = get_viewport().get_visible_rect()
 	spawnPoints[0] = Vector2( screenSize.x/4, 3*screenSize.y/4 ) #SouthWest
 	spawnPoints[1] = Vector2( 3*screenSize.x/4, 3*screenSize.y/4 ) #SouthEast
 	spawnPoints[2] = Vector2( screenSize.x/4, screenSize.y/4 ) #NorthWest
 	spawnPoints[3] = Vector2( 3*screenSize.x/4, screenSize.y/4 ) #NorthEast
-	var rand = RandomNumberGenerator.new()
-	#faire le spawn au point random ?
-	pass # Replace with function body.
-
+	var newPosition = rand.randi_range(0,3)
+	
+	var mob = mob_scene.instantiate()
+	mob.globalPosition = spawnPoints[newPosition]
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

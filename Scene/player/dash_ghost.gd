662a47b3bc64d2546527c7dea2505eb281768b77
SunPi <biometala@gmail.com>
2025-05-05 12:00:07 +0200

    Afterimage got cooler

--- Scene/player/dash_ghost.gd
+++ Scene/player/dash_ghost.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 
 func _ready():
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.5)
+	tween.tween_property(self, "modulate", Color.hex(0xffc2b378), 0.1)
+	tween.tween_property(self, "modulate", Color.hex(0x8c72c278), 0.2)
+	tween.tween_property(self, "modulate", Color.hex(0x8c72c200), 0.2)
 	tween.tween_callback(self.queue_free).set_delay(1)
 	pass
